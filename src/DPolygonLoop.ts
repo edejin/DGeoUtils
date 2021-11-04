93e094b62d4f1ac396361e141ab0f8b77fcf2f2c
Efim Dezhin <efim.dezhin@g42.ai>
2021-11-04 16:38:20 +0400

    fix loop

--- src/DPolygonLoop.ts
+++ src/DPolygonLoop.ts
@@ -3,187 +3,398 @@ import {DPoint, SetterFunction} from './DPoint';
 
 export type LoopFunction = (k: DPoint) => DPoint;
 
+enum LoopFunctions {
+  getTileFromCoords,
+  getCoordsFromTile,
+  height,
+  setX,
+  setY,
+  rotate,
+  move,
+  round,
+  ceil,
+  floor,
+  toFixed,
+  abs,
+  scale,
+  divide,
+  degreeToRadians,
+  radiansToDegrees,
+  radiansToMeters,
+  metersToRadians,
+  hipPoint,
+  xPoint,
+  yPoint,
+  wPoint,
+  hPoint,
+  setIfLessThan,
+  minus,
+  degreeToMeters,
+  metersToDegree,
+  flipVertically
+}
+
+interface PoolRecord {
+  functionName: LoopFunctions,
+  numberArg?: number;
+  setterArg?: number | SetterFunction;
+  pointArg?: DPoint;
+  numberPointArg?: number | DPoint;
+}
+
+// eslint-disable-next-line complexity
+const decodePoolRecord = (a: LoopFunction, {
+  functionName,
+  pointArg,
+  numberPointArg,
+  numberArg,
+  setterArg
+}: PoolRecord): LoopFunction => {
+  let res = a;
+  switch (functionName) {
+    case LoopFunctions.getTileFromCoords:
+      res = (k: DPoint): DPoint => a(k)
+        .getTileFromCoords(numberArg);
+      break;
+    case LoopFunctions.getCoordsFromTile:
+      res = (k: DPoint): DPoint => a(k)
+        .getCoordsFromTile(numberArg);
+      break;
+    case LoopFunctions.height:
+      res = (k: DPoint): DPoint => a(k)
+        .height(numberArg!);
+      break;
+    case LoopFunctions.setX:
+      res = (k: DPoint): DPoint => a(k)
+        .setX(setterArg!);
+      break;
+    case LoopFunctions.setY:
+      res = (k: DPoint): DPoint => a(k)
+        .setY(setterArg!);
+      break;
+    case LoopFunctions.rotate:
+      res = (k: DPoint): DPoint => a(k)
+        .rotate(numberArg!);
+      break;
+    case LoopFunctions.move:
+      res = (k: DPoint): DPoint => a(k)
+        .move(numberPointArg, numberArg);
+      break;
+    case LoopFunctions.round:
+      res = (k: DPoint): DPoint => a(k)
+        .round();
+      break;
+    case LoopFunctions.ceil:
+      res = (k: DPoint): DPoint => a(k)
+        .ceil();
+      break;
+    case LoopFunctions.floor:
+      res = (k: DPoint): DPoint => a(k)
+        .floor();
+      break;
+    case LoopFunctions.toFixed:
+      res = (k: DPoint): DPoint => a(k)
+        .toFixed(numberArg);
+      break;
+    case LoopFunctions.abs:
+      res = (k: DPoint): DPoint => a(k)
+        .abs();
+      break;
+    case LoopFunctions.scale:
+      res = (k: DPoint): DPoint => a(k)
+        .scale(numberPointArg, numberArg);
+      break;
+    case LoopFunctions.divide:
+      res = (k: DPoint): DPoint => a(k)
+        .divide(numberPointArg, numberArg);
+      break;
+    case LoopFunctions.degreeToRadians:
+      res = (k: DPoint): DPoint => a(k)
+        .degreeToRadians();
+      break;
+    case LoopFunctions.radiansToDegrees:
+      res = (k: DPoint): DPoint => a(k)
+        .radiansToDegrees();
+      break;
+    case LoopFunctions.radiansToMeters:
+      res = (k: DPoint): DPoint => a(k)
+        .radiansToMeters();
+      break;
+    case LoopFunctions.metersToRadians:
+      res = (k: DPoint): DPoint => a(k)
+        .metersToRadians();
+      break;
+    case LoopFunctions.hipPoint:
+      res = (k: DPoint): DPoint => a(k)
+        .hipPoint;
+      break;
+    case LoopFunctions.xPoint:
+      res = (k: DPoint): DPoint => a(k)
+        .xPoint;
+      break;
+    case LoopFunctions.yPoint:
+      res = (k: DPoint): DPoint => a(k)
+        .yPoint;
+      break;
+    case LoopFunctions.wPoint:
+      res = (k: DPoint): DPoint => a(k)
+        .wPoint;
+      break;
+    case LoopFunctions.hPoint:
+      res = (k: DPoint): DPoint => a(k)
+        .hPoint;
+      break;
+    case LoopFunctions.setIfLessThan:
+      res = (k: DPoint): DPoint => a(k)
+        .setIfLessThan(pointArg!);
+      break;
+    case LoopFunctions.minus:
+      res = (k: DPoint): DPoint => a(k)
+        .minus();
+      break;
+    case LoopFunctions.degreeToMeters:
+      res = (k: DPoint): DPoint => a(k)
+        .degreeToMeters();
+      break;
+    case LoopFunctions.metersToDegree:
+      res = (k: DPoint): DPoint => a(k)
+        .metersToDegree();
+      break;
+    case LoopFunctions.flipVertically:
+      res = (k: DPoint): DPoint => a(k)
+        .flipVertically(numberPointArg!);
+      break;
+    default:
+  }
+  return res;
+};
+
 export class DPolygonLoop {
-  private f: LoopFunction = (k: DPoint): DPoint => k;
+  private pool: PoolRecord[];
 
-  private readonly parent: DPolygon;
+  constructor(private readonly parent: DPolygon) {
+    this.pool = [];
+  }
 
-  constructor(parent: DPolygon) {
-    this.parent = parent;
+  private getLoopFunction() {
+    return this.pool.reduce(decodePoolRecord, (k: DPoint): DPoint => k);
   }
 
   /**
    * Run loop
    */
-  run(f: LoopFunction = this.f): DPolygon {
-    return this.parent.map(f);
+  run(): DPolygon {
+    return this.parent.map(this.getLoopFunction());
   }
 
   getTileFromCoords(zoom?: number): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).getTileFromCoords(zoom);
+    this.pool.push({
+      functionName: LoopFunctions.getTileFromCoords,
+      numberArg: zoom
+    });
     return this;
   }
 
   getCoordsFromTile(zoom?: number): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).getCoordsFromTile(zoom);
+    this.pool.push({
+      functionName: LoopFunctions.getCoordsFromTile,
+      numberArg: zoom
+    });
     return this;
   }
 
   height(z: number): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).height(z);
+    this.pool.push({
+      functionName: LoopFunctions.height,
+      numberArg: z
+    });
     return this;
   }
 
   setX(x: number | SetterFunction): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).setX(x);
+    this.pool.push({
+      functionName: LoopFunctions.setX,
+      setterArg: x
+    });
     return this;
   }
 
   setY(y: number | SetterFunction): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).setY(y);
+    this.pool.push({
+      functionName: LoopFunctions.setY,
+      setterArg: y
+    });
     return this;
   }
 
   rotate(a: number): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).rotate(a);
+    this.pool.push({
+      functionName: LoopFunctions.rotate,
+      numberArg: a
+    });
     return this;
   }
 
-  move(x: number | DPoint = 0, y?: number): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).move(x, y);
+  move(x: number | DPoint = 0, y: number = (x as number)): DPolygonLoop {
+    this.pool.push({
+      functionName: LoopFunctions.move,
+      numberPointArg: x,
+      numberArg: y
+    });
     return this;
   }
 
   round(): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).round();
+    this.pool.push({
+      functionName: LoopFunctions.round
+    });
     return this;
   }
 
   ceil(): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).ceil();
+    this.pool.push({
+      functionName: LoopFunctions.ceil
+    });
     return this;
   }
 
   floor(): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).floor();
+    this.pool.push({
+      functionName: LoopFunctions.floor
+    });
     return this;
   }
 
   toFixed(n: number = 2): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).toFixed(n);
+    this.pool.push({
+      functionName: LoopFunctions.toFixed,
+      numberArg: n
+    });
     return this;
   }
 
   abs(): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).abs();
+    this.pool.push({
+      functionName: LoopFunctions.abs
+    });
     return this;
   }
 
-  scale(x: number | DPoint = 0, y?: number): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).scale(x, y);
+  scale(x: number | DPoint = 0, y: number = (x as number)): DPolygonLoop {
+    this.pool.push({
+      functionName: LoopFunctions.scale,
+      numberPointArg: x,
+      numberArg: y
+    });
     return this;
   }
 
   divide(x: number | DPoint = 0, y?: number): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).divide(x, y);
+    this.pool.push({
+      functionName: LoopFunctions.divide,
+      numberPointArg: x,
+      numberArg: y
+    });
     return this;
   }
 
   degreeToRadians(): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).degreeToRadians();
+    this.pool.push({
+      functionName: LoopFunctions.degreeToRadians
+    });
     return this;
   }
 
   radiansToDegrees(): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).radiansToDegrees();
+    this.pool.push({
+      functionName: LoopFunctions.radiansToDegrees
+    });
     return this;
   }
 
   radiansToMeters(): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).radiansToMeters();
+    this.pool.push({
+      functionName: LoopFunctions.radiansToMeters
+    });
     return this;
   }
 
   metersToRadians(): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).metersToRadians();
+    this.pool.push({
+      functionName: LoopFunctions.metersToRadians
+    });
     return this;
   }
 
   getHipPoint(): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).hipPoint;
+    this.pool.push({
+      functionName: LoopFunctions.hipPoint
+    });
     return this;
   }
 
   getXPoint(): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).xPoint;
+    this.pool.push({
+      functionName: LoopFunctions.xPoint
+    });
     return this;
   }
 
   getYPoint(): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).yPoint;
+    this.pool.push({
+      functionName: LoopFunctions.yPoint
+    });
     return this;
   }
 
   getWPoint(): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).wPoint;
+    this.pool.push({
+      functionName: LoopFunctions.wPoint
+    });
     return this;
   }
 
   getHPoint(): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).hPoint;
+    this.pool.push({
+      functionName: LoopFunctions.hPoint
+    });
     return this;
   }
 
   setIfLessThan(p: DPoint): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).setIfLessThan(p);
+    this.pool.push({
+      functionName: LoopFunctions.setIfLessThan,
+      pointArg: p
+    });
     return this;
   }
 
   minus(): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).minus();
+    this.pool.push({
+      functionName: LoopFunctions.minus
+    });
     return this;
   }
 
   degreeToMeters(): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).degreeToMeters();
+    this.pool.push({
+      functionName: LoopFunctions.degreeToMeters
+    });
     return this;
   }
 
   metersToDegree(): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).metersToDegree();
+    this.pool.push({
+      functionName: LoopFunctions.metersToDegree
+    });
     return this;
   }
 
   flipVertically(size: DPoint | number): DPolygonLoop {
-    const t = this.f.bind(null);
-    this.f = (k: DPoint) => t(k).flipVertically(size);
+    this.pool.push({
+      functionName: LoopFunctions.flipVertically,
+      numberPointArg: size
+    });
     return this;
   }
 }
