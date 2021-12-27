a30dc585be9d17e81314945a4ecb5de3ed189b47
Efim Dezhin <efim.dezhin@g42.ai>
2021-12-27 14:23:31 +0400

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,21 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/tests"
-  ],
-  "transform": {
-    "^.+\\.ts$": "ts-jest"
+  roots: ['<rootDir>/tests'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
   },
-  "coverageDirectory": "media",
-  "coverageReporters": [
-    "json-summary",
-    "text",
-    "lcov"
+  coverageDirectory: 'media',
+  coverageReporters: [
+    'json-summary',
+    'text',
+    'lcov'
   ],
-  "reporters": [
-    "default",
-    ["./node_modules/jest-html-reporter", {
-    "outputPath": "./media/time-report.html"
-    }]
+  reporters: [
+    'default',
+    [
+      './node_modules/jest-html-reporter',
+      {
+        outputPath: './media/time-report.html'
+      }
+    ]
   ]
-}
+};
